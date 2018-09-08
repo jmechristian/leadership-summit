626e6b9c0064712cac9d6cdacc53b742107afb8c
JMEChristian <jmechristian@gmail.com>
2018-09-07 21:19:49 -0500

    finish header and side drawer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/layout/Header";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App" style={{ height: "1500px" }}>
         <Header />
       </div>
     );
